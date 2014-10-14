998330a8f045fb7ad8d35f77a7503023165852ec
aaronBeryUcl <a.bery@ucl.ac.uk>
2014-10-14 23:54:03 +0100

    add pricing page and styles
    add pricing page and styles

---
+++ price-template.php
@@ -0,0 +1,20 @@
+<?php
+/*
+Template Name: Pricing
+*/
+?>
+<?php get_header(); ?>
+    <div id="main">
+      <div class="box">
+          <?php 
+            if ( have_posts() ) : while ( have_posts() ) : the_post();
+          ?>
+          <div class="box--header"><?php echo the_title(); ?></div>
+          <?php
+            the_content();
+            endwhile; endif;
+          ?>
+      </div>
+    </div>
+<?php //get_sidebar(); ?>
+<?php get_footer(); ?>
